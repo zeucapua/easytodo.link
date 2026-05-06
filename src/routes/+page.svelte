052d28e6c65f956d61b82d1a255d6f4d36b643d6
zeudev <coding@zeu.dev>
2026-05-06 06:35:23 -0700

    init default user pref with theme + openPinOnLoad, implemenet openPinOnLoad, change coming soon feats and style in layout menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<p>Test</p>
+<script lang="ts">
+  import { user_preferences } from "$lib/stores.svelte";
+</script>
+
+<label for="openPinOnLoad">
+  <input id="openPinOnLoad" type="checkbox" bind:checked={user_preferences.current.openPinOnLoad} />
+  Open pinned list on load
+</label>
